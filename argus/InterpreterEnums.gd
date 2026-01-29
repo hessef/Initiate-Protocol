783c67c5d49561b7104687b5f486cc48f69628e8
hessef <35468516+hessef@users.noreply.github.com>
2026-01-28 19:04:07 -0600

    added basic print, addition, and subtraction functionality

---
+++ argus/InterpreterEnums.gd
@@ -0,0 +1,10 @@
+extends Node
+
+class_name ArgusEnum
+
+enum data_types{
+	INT,
+	#FLT
+	STR
+	
+}
---
+++ argus/ProtRunner.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name ProtRunner
+
+#default path for testing
+@export var prot_path: String = "res://scripts/test.prot"
+
+func _ready() -> void:
+	run_file(prot_path)
+
+func run_file(path: String) -> void:
+	if not FileAccess.file_exists(path):
+		push_error("Protocol file not found: %s" % path)
+		return
+
+	var f := FileAccess.open(path, FileAccess.READ)
+	var source := f.get_as_text()
+
+	var interp := ProtInterpreter.new()
+	interp.output = func(msg):
+		#TODO: swap this to output to the in-game terminal
+		print("[PROT] ", msg)
+
+	interp.run_source(source)
